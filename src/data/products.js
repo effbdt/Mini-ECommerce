e03111d2ddbd7683df74a726fc97ee5dc17d4934
David <szadav09@gmail.com>
2026-03-26 13:51:12 +0100

    added 10 items as mock data

---
+++ src/data/products.js
@@ -0,0 +1,93 @@
+export const products = [
+    {
+        id: 1,
+        name: "Shoes",
+        price: 50,
+        category: "Shoes",
+        inStock: true,
+        rating: 4,
+        image: "https://i5.walmartimages.com/seo/Damyuan-Running-Shoes-Men-Fashion-Sneakers-Casual-Walking-Shoes-Sport-Athletic-Shoes-Lightweight-Breathable-Comfortable_4114141f-7d26-4dd7-933d-babc24080395.516ad145e1a1d8d82a801ac48231950d.jpeg"
+    },
+    {
+        id: 2,
+        name: "Laptop",
+        price: 900,
+        category: "Electronics",
+        inStock: true,
+        rating: 5,
+        image: "https://media.istockphoto.com/id/1389603578/photo/laptop-blank-screen-on-wood-table-with-blurred-coffee-shop-cafe-interior-background-and.jpg?s=612x612&w=0&k=20&c=bPf3XxUZJZ6HRw7BE75ur1wBMCm_r4QAr-_lajERIyU="
+    },
+    {
+        id: 3,
+        name: "Phone",
+        price: 600,
+        category: "Electronics",
+        inStock: false,
+        rating: 4,
+        image: "https://t4.ftcdn.net/jpg/02/97/35/11/360_F_297351123_gd71TTj4GzWtalft9fIQF8XcqsOnwj8S.jpg"
+    },
+    {
+        id: 4,
+        name: "T-shirt",
+        price: 20,
+        category: "Clothing",
+        inStock: true,
+        rating: 3,
+        image: "https://st.depositphotos.com/1017986/2626/i/450/depositphotos_26263587-stock-photo-man-in-blank-t-shirt.jpg"
+    },
+    {
+        id: 5,
+        name: "Headphones",
+        price: 120,
+        category: "Electronics",
+        inStock: true,
+        rating: 4,
+        image: "https://media.istockphoto.com/id/1412240771/photo/headphones-on-white-background.jpg?s=612x612&w=0&k=20&c=DwpnlOcMzclX8zJDKOMSqcXdc1E7gyGYgfX5Xr753aQ="
+    },
+    {
+        id: 6,
+        name: "Sneakers",
+        price: 80,
+        category: "Shoes",
+        inStock: true,
+        rating: 5,
+        image: "https://media.istockphoto.com/id/1688015574/photo/white-sneaker-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=gz8bGn7h_eaF4uJGJjdZYYhJDrrigHAygo2Vi8tZjH8="
+    },
+    {
+        id: 7,
+        name: "Jacket",
+        price: 150,
+        category: "Clothing",
+        inStock: false,
+        rating: 4,
+        image: "https://static8.depositphotos.com/1559986/1051/i/450/depositphotos_10513998-stock-photo-red-jacket.jpg"
+    },
+    {
+        id: 8,
+        name: "Watch",
+        price: 200,
+        category: "Accessories",
+        inStock: true,
+        rating: 5,
+        image: "https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?cs=srgb&dl=pexels-ferarcosn-190819.jpg&fm=jpg"
+    },
+    {
+        id: 9,
+        name: "Backpack",
+        price: 70,
+        category: "Accessories",
+        inStock: true,
+        rating: 2,
+        image: "https://media.istockphoto.com/id/2167589456/photo/yellow-backpack-opened-isolated-on-white-school-bag-advertisement-design-knapsack-rucksack.jpg?s=612x612&w=0&k=20&c=5B7A5M35ZEcneBbRoLWkIpPcij1HKTdPtkXEm71MMKI="
+
+    },
+    {
+        id: 10,
+        name: "Sunglasses",
+        price: 100,
+        category: "Accessories",
+        inStock: false,
+        rating: 1,
+        image: "https://img.freepik.com/free-psd/elegant-black-gold-sunglasses-stylish-accessory_191095-79382.jpg?semt=ais_hybrid&w=740&q=80"
+    }
+]
