95285a08aaec9b2eaaeec2c9d4c286b2279bdc54
David <szadav09@gmail.com>
2026-03-26 14:00:32 +0100

    The products show up on the main page, created the component files

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup>
-
+import { products } from './data/products';
 </script>
 
 <template>
+  <h1>Products</h1>
+
+  <ul>
+    <li v-for="p in products" :key="p.id">
+      {{ p.name }} - {{ p.price }}
+    </li>
+  </ul>
 
 </template>
 
