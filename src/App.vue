f818647a33f6bcb4139d7efdba8d2d48825b1876
David <szadav09@gmail.com>
2026-03-26 14:25:18 +0100

    Created a basic product card and implemented into the main page

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
 import { products } from './data/products';
+import ProductCard from './components/ProductCard.vue';
 </script>
 
 <template>
   <h1>Products</h1>
 
-  <ul>
-    <li v-for="p in products" :key="p.id">
-      {{ p.name }} - {{ p.price }}
-    </li>
-  </ul>
+  <ProductCard v-for="p in products" :key="p.id" :product="p" />
 
 </template>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    product: Object
+})
+</script>
+
+<template>
+    <div>
+        <img :src="product.image" width="120" />
+
+        <h3>{{ product.name }}</h3>
+        <p>Price: ${{ product.price }}</p>
+        <p>Category: {{ product.category }}</p>
+        <p>Rating: *{{ product.rating }}</p>
+
+        <button :disabled="!product.inStock">
+            {{ product.inStock ? "Add to Cart" : "Out of stock" }}
+        </button>
+    </div>
+
+</template>
